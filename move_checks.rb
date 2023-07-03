0ba4b070244a6deecbba52d0dd734838397a40c9
Christian <christianvillere@gmail.com>
2023-07-02 20:45:40 -0700

    Add move checks for bishops, rocks, and queens

--- move_checks.rb
+++ move_checks.rb
@@ -5,11 +5,15 @@ module MoveChecks
 
   KNIGHT_MOVES = [[2, -1], [2, 1], [1, -2], [1, 2], [-1, -2], [-1, 2], [-2, -1], [-2, 1]].freeze
   BISHOP_PATHS = [[1, 1], [1, -1], [-1, -1], [-1, 1]].freeze
+  ROOK_MOVES = [[1, 0], [0, 1], [-1, 0], [0, -1]].freeze
+  QUEEN_MOVES = [[1, 1], [1, -1], [-1, -1], [-1, 1], [1, 0], [0, 1], [-1, 0], [0, -1]].freeze
 
   def deter_piece_check(start_pos, end_pos, obj, piece)
     return check_back_row(start_pos, end_pos, obj, piece) if piece.is_a? GamePieces::Pawn
     return check_knight_move(start_pos, end_pos, obj, piece) if piece.is_a? GamePieces::Knight
     return check_bishop_move(start_pos, end_pos, obj, piece) if piece.is_a? GamePieces::Bishop
+    return check_rook_move(start_pos, end_pos, obj, piece) if piece.is_a? GamePieces::Rook
+    return check_queen_move(start_pos, end_pos, obj, piece) if piece.is_a? GamePieces::Queen
   end
 
   def check_back_row(start_pos, end_pos, obj, piece)
@@ -55,18 +59,36 @@ module MoveChecks
 
   def check_bishop_move(start_pos, end_pos, obj, _piece)
     return false if (start_pos[0] - end_pos[0] == 0 || start_pos[1] - end_pos[1] == 0)
-    if bishop_patt(start_pos, end_pos) == false
+    if patt_finder(start_pos, end_pos, MoveChecks::BISHOP_PATHS) == false
       return false
     else
-      return check_pieces_between(start_pos, end_pos, obj, bishop_patt(start_pos, end_pos))
+      return check_pieces_between(start_pos, end_pos, obj, patt_finder(start_pos, end_pos, MoveChecks::BISHOP_PATHS))
     end
   end
 
-  def bishop_patt(start_pos, end_pos)
-    BISHOP_PATHS.each do |n|
+  def check_rook_move(start_pos, end_pos, obj, _piece)
+    return false if (start_pos[0] - end_pos[0] == 0 && start_pos[1] - end_pos[1] == 0)
+    if patt_finder(start_pos, end_pos, MoveChecks::ROOK_MOVES) == false
+      return false
+    else
+      return check_pieces_between(start_pos, end_pos, obj, patt_finder(start_pos, end_pos, MoveChecks::ROOK_MOVES))
+    end
+  end
+
+  def check_queen_move(start_pos, end_pos, obj, _piece)
+    return false if (start_pos[0] - end_pos[0] == 0 && start_pos[1] - end_pos[1] == 0)
+    if patt_finder(start_pos, end_pos, MoveChecks::QUEEN_MOVES) == false
+      return false
+    else
+      return check_pieces_between(start_pos, end_pos, obj, patt_finder(start_pos, end_pos, MoveChecks::QUEEN_MOVES))
+    end
+  end
+
+  def patt_finder(start_pos, end_pos, patt)
+    patt.each do |n|
       new_row_pos = start_pos[0]
       new_col_pos = start_pos[1]
-      until new_row_pos >= 9 || new_col_pos >= 9
+      7.times do 
         new_row_pos += n[0]
         new_col_pos += n[1]
         return n if [new_row_pos, new_col_pos] == [end_pos[0], end_pos[1]]
@@ -75,18 +97,16 @@ module MoveChecks
     return false
   end
 
-  def check_pieces_between(start_pos, end_pos, obj, bishop_patt)
+  def check_pieces_between(start_pos, end_pos, obj, patt)
     loop_val = (start_pos[0] - end_pos[0]).abs - 1
     new_row_pos = start_pos[0]
     new_col_pos = start_pos[1]
     loop_val.times do
-      new_row_pos += bishop_patt[0]
-      new_col_pos += bishop_patt[1]
+      new_row_pos += patt[0]
+      new_col_pos += patt[1]
       return false if obj.board[new_row_pos][new_col_pos] != "\u25AA"
     end
     true
   end
-
-
 
 end
